68ca9c954213489fc37185f14e691f4d32cc0d1e
djavid <dpkhalilov@edu.hse.ru>
2017-12-01 18:40:02 +0300

    make it better

--- src/main/java/com/djavid/br_server/model/entity/RegistrationToken.java
+++ src/main/java/com/djavid/br_server/model/entity/RegistrationToken.java
@@ -1,7 +1,10 @@
 package com.djavid.br_server.model.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -9,7 +12,7 @@ import javax.persistence.Id;
 public class RegistrationToken {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
     public String token;
 
